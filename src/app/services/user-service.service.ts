847923a1e3d2925f2ebae71548e07a3d8d5ef694
shivam <0910shivamsingh@gmail.com>
2021-04-21 00:38:38 -0700

    transaction

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Status } from '../Models/status';
+import { Transaction } from '../Models/transaction';
 import { User } from '../Models/user';
 
 @Injectable({
@@ -17,5 +18,10 @@ export class UserServiceService {
 
   activateCard(userId:number):Observable<Status>{
     return this.httpclient.get<Status>("http://localhost:9090/activatecard?userId="+userId);
+  }
+
+  getTransactionByUserId(userId:number):Observable<Transaction[]>{
+    return this.httpclient.get<Transaction[]>("http://localhost:9090/transactionsbyuserid?userId="+userId);
+    
   }
 }
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { LoginStatus } from '../Models/login-status';
+import { Transaction } from '../Models/transaction';
+import { UserServiceService } from '../services/user-service.service';
 
 @Component({
   selector: 'app-transactions',
@@ -8,84 +11,38 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./transactions.component.css'],
 })
 export class TransactionsComponent implements OnInit {
+  
+  transacts:Transaction[]=[];
+  loguser:LoginStatus;
+
+  constructor(private uservice:UserServiceService){}
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatPaginator) dataSource: MatTableDataSource<Transaction>;
+
+  
+
+  ngOnInit() {
+    // this.dataSource.paginator = this.paginator;
+    this.loguser= JSON.parse(localStorage.getItem("loggedinuser"));
+    this.uservice.getTransactionByUserId(this.loguser.userId).subscribe(
+      data=>{
+         console.log(JSON.stringify(data))
+        this.transacts=data;
+        // console.log(this.transacts);
+        this.dataSource = new MatTableDataSource(this.transacts);
+          this.dataSource.paginator = this.paginator;
+      }
+    );
+
+  }
+
   displayedColumns: string[] = [
     'TransactionId',
     'Date',
     'Amount',
     'TransactionMessage',
   ];
-  dataSource = new MatTableDataSource<transactions>(TRANSACTION_LIST);
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-  }
 }
-export interface transactions {
-  TransactionId: number;
-  Amount: number;
-  Date: string;
-  TransactionMessage: string;
-}
-const TRANSACTION_LIST: transactions[] = [
-  {
-    TransactionId: 257,
-    Date: '01 - 05 - 20',
-    Amount: 10000,
-    TransactionMessage: 'Amount deducted for EMI installment',
-  },
-  {
-    TransactionId: 458,
-    Date: '05 - 08 - 16',
-    Amount: 15000,
-    TransactionMessage: 'Amount deducted for EMI installment',
-  },
-  {
-    TransactionId: 128,
-    Date: '07 - 01 - 12',
-    Amount: 25000,
-    TransactionMessage: 'Amount deducted for EMI installment',
-  },
-  {
-    TransactionId: 613,
-    Date: '04 - 05 - 21',
-    Amount: 8000,
-    TransactionMessage: 'Amount deducted for card activation',
-  },
-  {
-    TransactionId: 778,
-    Date: '22 - 01 - 17',
-    Amount: 10000,
-    TransactionMessage: 'Amount deducted for card EMI installment',
-  },
-  {
-    TransactionId: 984,
-    Date: '03 - 06 - 12',
-    Amount: 5000,
-    TransactionMessage: 'Amount deducted for card EMI installment',
-  },
-  {
-    TransactionId: 146,
-    Date: '14 - 01 - 18',
-    Amount: 9000,
-    TransactionMessage: 'Amount deducted for card EMI installment',
-  },
-  {
-    TransactionId: 257,
-    Date: '06 - 05 -20',
-    Amount: 6000,
-    TransactionMessage: 'Amount deducted for EMI installment',
-  },
-  {
-    TransactionId: 114,
-    Date: '17 - 08 - 21',
-    Amount: 3500,
-    TransactionMessage: 'Amount deducted for card activation',
-  },
-  {
-    TransactionId: 900,
-    Date: '16 - 05 - 14',
-    Amount: 3000,
-    TransactionMessage: 'Amount deducted for card activation',
-  },
-];
+
+
